f43d32ec010cc7f7c3bcdba8f2f0ddbf5211ceb0
BuildTools <unconfigured@null.spigotmc.org>
2018-11-26 00:44:38 +0100

    Kostra - delitel prikazov

--- main.c
+++ main.c
@@ -2,6 +2,26 @@
 
 int main()
 {
+    int exit = 1;
+    char prikaz;
+
+    do
+    {
+        scanf("%1c", &prikaz);
+
+        switch(prikaz) // Deli prikazy
+            // nie je cas hladat lepsi sposob
+        {
+            case 'n': break; // TODO nacitanie suboru
+            case 'v': break; // TODO vypis suboru
+            case 'h': break; // TODO vyhladavanie
+            case 'p': break; // TODO pridavanie
+            case 'z': break; // TODO mazanie
+            case 'a': break; // TODO aktualizacia zaznamu
+            case 'k':  exit = 0; break; // TODO uvolnenie pamate
+        }
+    }
+    while(exit);
 
     return 0;
 }
